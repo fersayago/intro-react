b7c19484291b721e748600a357a8b415b7231104
fersayago <sayago.fernando@gmail.com>
2021-11-27 12:13:59 -0300

    creado esqueleto de todo-machine

---
+++ todo-machine/src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TodoCounter/>
+      <TodoSearch/>
+      <TodoList>
+        <TodoItem/>
+      </TodoList>
+      <CreateTodoButton/>
+    </div>
+  );
+}
+
+export default App;
