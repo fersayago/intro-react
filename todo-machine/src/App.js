c2ef7f2316df92ba48d68683a80d6c0d3bd38dda
fersayago <sayago.fernando@gmail.com>
2021-12-08 18:32:05 -0300

    Manejo de efectos (eventos externos)

--- todo-machine/src/App.js
+++ todo-machine/src/App.js
@@ -11,35 +11,65 @@ const defaultTodos = [
 */
 
 function useLocalStorage (itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName)
-  let parsedItem;
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
 
-  
-  if (!localStorageItem){
-    // si localStorageItem esta vacio se crea un array el cual luego se puede poblar y
-    // utilizar con JSON.parse en el estado de Item
-    localStorage.setItem(itemName, JSON.stringify(initialValue))
-    parsedItem= [];
-  } else {
-    parsedItem = JSON.parse(localStorageItem)
-  }
+  React.useEffect(() => {
+    // simulamos el tiempo de espera del local storate como 
+    // si se tratase de consumir una API
+    setTimeout(() => {
+      try{
+        const localStorageItem = localStorage.getItem(itemName)
+        let parsedItem;
 
-  const [item, setItem] = React.useState(parsedItem);
+        if (!localStorageItem){
+          // si localStorageItem esta vacio se crea un array el cual luego se puede poblar y
+          // utilizar con JSON.parse en el estado de Item
+          localStorage.setItem(itemName, JSON.stringify(initialValue))
+          parsedItem= [];
+        } else {
+          parsedItem = JSON.parse(localStorageItem)
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      } catch(error) {
+        setError(error);
+      }
+    },1000)
+  })
   
   // funcion para guardar los Item en el estado pero ademas
   // PARA PERSISTIR LOS ITEM EN EL LOCAL STORAGE
   const saveItem= (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error){
+      setError(error);
+    }
   }
 
-  return [ item, saveItem ]
+  return {
+    item,
+    saveItem,
+    loading,
+    error
+  }
 }
 
 function App() {
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  // cambiamos para en vez de recibir array, recibir un objeto y luego lo
+  // renombramos para que los nombres coincidan
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => todo.completed === true).length;
@@ -81,7 +111,9 @@ function App() {
   }
 
   return (
-    <AppUI 
+    <AppUI
+      loading={loading}
+      error={error}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
--- todo-machine/src/AppUI.js
+++ todo-machine/src/AppUI.js
@@ -6,6 +6,8 @@ import TodoItem from './components/TodoItem';
 import CreateTodoButton from './components/CreateTodoButton';
 
 function AppUI ({
+  error,
+  loading,
   totalTodos,
   completedTodos,
   searchValue,
@@ -27,6 +29,9 @@ function AppUI ({
     setSearchValue={setSearchValue}
     />
   <TodoList>
+    {error && <p>Hubo un error, desespérate</p>}
+    {loading && <p>Éstamos cargando, no desesperes...</p>}
+    {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
     {searchedTodos.map(todo => (
       <TodoItem
         key={todo.text}
